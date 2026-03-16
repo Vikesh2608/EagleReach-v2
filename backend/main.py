baf2a9b027561b231184c723f3aac69c3c1a6e21
Vikesh Sagar Bairam <142835187+Vikesh2608@users.noreply.github.com>
2026-03-16 05:31:10 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,13 @@ def weather():
     lat = 40.7128
     lon = -74.0060
 
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min&timezone=auto"
+    url = (
+        "https://api.open-meteo.com/v1/forecast"
+        "?latitude=40.7128"
+        "&longitude=-74.0060"
+        "&daily=weathercode,temperature_2m_max,temperature_2m_min"
+        "&timezone=auto"
+    )
 
     r = requests.get(url)
 
