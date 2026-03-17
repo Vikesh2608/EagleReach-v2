4f6fca58909a03a4ab06321fc3d0bb1bed01cf22
Vikesh Sagar Bairam <142835187+Vikesh2608@users.noreply.github.com>
2026-03-16 22:27:18 -0400

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -10,9 +10,27 @@ async function searchZip() {
 
     let html = "";
 
-    data.results.forEach(rep => {
-        html += `<p><b>${rep.name}</b> (${rep.party})</p>`;
+    // Governor
+    html += `<h3>Governor</h3><p>${data.governor}</p>`;
+
+    // Mayor
+    html += `<h3>Mayor</h3><p>${data.mayor}</p>`;
+
+    // Representatives
+    html += `<h3>Federal Leaders</h3>`;
+
+    data.representatives.forEach(rep => {
+        html += `
+            <div style="border:1px solid #ccc; padding:10px; margin:10px;">
+                <b>${rep.name}</b><br>
+                Party: ${rep.party}<br>
+                Phone: ${rep.phone}<br>
+                <a href="${rep.link}" target="_blank">Website</a>
+            </div>
+        `;
     });
 
     document.getElementById("leaders").innerHTML = html;
+
+    loadNews();
 }
